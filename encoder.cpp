f5ff6145fe75e00b20b7cfef44b534708115ef2e
LarryWay <larryway15@gmail.com>
2023-08-21 11:24:04 -0400

    FIxed _required_bits_to_translate function

--- encoder.cpp
+++ encoder.cpp
@@ -19,20 +19,20 @@ namespace mrse{
         for(const char c : input){
             switch(c){
                 case '.': counter++; break;     // counts bits required for a '.' (1)
-                case '-': counter += 2; break;  // counts bits required for a '-' (2)
-                case ' ': counter += 2; break;  // counts bits required for a ' ' (2)
-                case '/': counter += 3; break;  // counts bits required for a '/' (3)
+                case '-': counter += 2 ; break;  // counts bits required for a '-' (2)
+                case ' ': counter += 1 ; break;  // counts bits required for a ' ' (2)
+                case '/': counter += 3 ; break;  // counts bits required for a '/' (3)
+                default: std::cout << "ERROR" << std::endl;
             }
-            counter += 2;  // counts bits for space between characters (1) 
+            counter++;  // counts bits for space between characters (1) 
         }
-
-        return (counter);  //  -1 to ignore last "space between characters", not needed
+        return (counter - 1);  //  -1 to ignore last "space between characters", not needed
     }
 
 
 
     int _num_of_dtypes_needed(float _required_bits){  // calculates number of d_Types needed to contain encoded message 
-        float bits_per_dtype = (_required_bits / (sizeof(d_Type) * 8));
+        float bits_per_dtype = (_required_bits / (sizeof(d_Type) * 8)) + 0.05;  // Add 0.05 to guarentee enough space for bits  (DOCUMENT)
         
         if(bits_per_dtype == floor(bits_per_dtype)){
             return bits_per_dtype;
@@ -55,7 +55,7 @@ namespace mrse{
                 stream = ((stream << 2) | 0b111) << 2;  // marks a '/' as a "111" in binary
             }
         }
-        stream = stream >> 1;
+        stream = stream >> 2;
     }
 
 
@@ -76,12 +76,14 @@ namespace mrse{
         }
         split_locs.at(sp_size - 1) = { block_size * (sp_size - 1)  , block_size * sp_size + block_remainder};
 
-        /*  
-        USE FOR DEBUG
+         
+        /*
+        DEBUG
         for(auto p : split_locs){
             std::cout << std::string{input.begin() + p.first, input.begin() + p.second} << '\t' << "(" << p.first << "," << p.second << ")" << '\n';
         }
         */
+        
 
         for(int x = 0 ; x < rv_size ; x++){
             _encode_by_bitshift(return_vec.at(x), {input.begin() + split_locs.at(x).first, input.begin() + split_locs.at(x).second});
