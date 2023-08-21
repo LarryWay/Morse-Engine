c9f05a135e6234635f1a9e1b0c624fbe17044d8b
LarryWay <larryway15@gmail.com>
2023-08-21 16:39:56 -0400

    changed encode_morse's split_locs algorithm

--- encoder.cpp
+++ encoder.cpp
@@ -1,12 +1,12 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <bitset>
 #include <cmath>
-#include <iostream>
 #include <utility>
 
-#include "dictionary.hpp"
 
 
 
@@ -14,11 +14,11 @@ namespace mrse{
 
     using d_Type = uint64_t;
 
-    int _required_bits_to_translate(const std::string& input){  // TODO: FIX THIS FUNCTION TO RETURN ACCURATE NUMBERS
+    int _required_bits_to_translate(const std::string& input){  
         int counter = 0;
         for(const char c : input){
             switch(c){
-                case '.': counter++; break;     // counts bits required for a '.' (1)
+                case '.': counter++; break;      // counts bits required for a '.' (1)
                 case '-': counter += 2 ; break;  // counts bits required for a '-' (2)
                 case ' ': counter += 1 ; break;  // counts bits required for a ' ' (2)
                 case '/': counter += 3 ; break;  // counts bits required for a '/' (3)
@@ -32,8 +32,8 @@ namespace mrse{
 
 
     int _num_of_dtypes_needed(float _required_bits){  // calculates number of d_Types needed to contain encoded message 
-        float bits_per_dtype = (_required_bits / (sizeof(d_Type) * 8)) + 0.05;  // Add 0.05 to guarentee enough space for bits  (DOCUMENT)
-        
+        float bits_per_dtype = (_required_bits / (sizeof(d_Type) * 8)) + 0.1;  // Add 0.1 to guarentee enough space for bits  (DOCUMENT)
+
         if(bits_per_dtype == floor(bits_per_dtype)){
             return bits_per_dtype;
         }
@@ -46,12 +46,12 @@ namespace mrse{
         
         for(char c : str){
             if(c == '.'){
-                stream = (stream | 0b1) << 2;  // marks a '.' as a "1" in binary
+                stream = (stream | 0b1) << 2;           // marks a '.' as a "1" in binary
             }else if(c == '-'){
-                stream = ((stream << 1) | 0b11) << 2;  // marks a '-' as a "11" in binary
+                stream = ((stream << 1) | 0b11) << 2;   // marks a '-' as a "11" in binary
             }else if(c == ' '){
-                stream = stream << 2;  // marks a ' ' as a "00" in binary
-            }else if(c == '/'){  // *** CAUTION: assumes a ' ' follows the '/' ***  SHOULD MAKE MORE RESILIANT
+                stream = stream << 2;                   // marks a ' ' as a "00" in binary
+            }else if(c == '/'){                         // *** CAUTION: assumes a ' ' follows the '/' ***  SHOULD MAKE MORE RESILIANT
                 stream = ((stream << 2) | 0b111) << 2;  // marks a '/' as a "111" in binary
             }
         }
@@ -61,28 +61,33 @@ namespace mrse{
 
 
     std::vector<d_Type> encode_morse(const std::string& input){
-        std::vector<d_Type> return_vec(_num_of_dtypes_needed(_required_bits_to_translate(input)));  // Increment by one to guarentee space
+        std::vector<d_Type> return_vec(_num_of_dtypes_needed(_required_bits_to_translate(input)));
         std::fill(return_vec.begin(), return_vec.end(), 0);
 
         const auto i_size = input.size();
         const auto rv_size = return_vec.size();
         const auto block_size = floor(i_size / rv_size);
         const auto block_remainder = i_size % rv_size;
+        auto cnt = block_remainder;
         
         std::vector<std::pair<int, int>> split_locs(rv_size);
         const auto sp_size = split_locs.size();
-        for(int x = 0 ; x < sp_size - 1 ; x++){
-            split_locs.at(x) = { block_size * x  ,  block_size * (x + 1)};
+        for(int x = 0 ; x < sp_size ; x++){
+            if(cnt){
+                split_locs.at(x) = { (block_size + 1) * x  ,  (block_size + 1) * (x + 1)};
+                cnt--;
+            }else{
+                split_locs.at(x) = { block_size * x + block_remainder  ,  block_size * (x + 1) + block_remainder};
+            }
+            
         }
-        split_locs.at(sp_size - 1) = { block_size * (sp_size - 1)  , block_size * sp_size + block_remainder};
-
          
-        /*
-        DEBUG
+        /*FOR DEBUGGS
         for(auto p : split_locs){
             std::cout << std::string{input.begin() + p.first, input.begin() + p.second} << '\t' << "(" << p.first << "," << p.second << ")" << '\n';
         }
         */
+        
         
 
         for(int x = 0 ; x < rv_size ; x++){
